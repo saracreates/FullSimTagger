816f97e93b70437bc44934ab196999c44528491f
Sara Aumiller <sara.aumiller@cern.ch>
2024-07-10 14:08:53 +0200

    work on batch commit

--- submit/submitAnalysisJob.sh
+++ submit/submitAnalysisJob.sh
@@ -2,12 +2,57 @@
 
 # shell script to submit analysis job to the batch system
 
-#source /cvmfs/sw.hsf.org/key4hep/setup.sh
-LOCAL_DIR=${1}
-SCRIPT=${2}
-INPUTFILE=${3}
-OUTPUTFILE=${4}
-NEVENTS=${5}
+# source key4ehp
+source /cvmfs/sw.hsf.org/key4hep/setup.sh -r 2024-04-12
+
+# get input parameters 
+FROM_I=${1} # Wwhich index to start with regarding the input root files
+NUM_FILES=${2} # number of files to process
+FILE_PATTERN=${3}
+OUTPUT_DIR=${4} # output directory
+OUTPUT_FILE=${5} # output file
+
+# get input files
+root_files=($(ls ${FILE_PATTERN} 2>/dev/null | sort | tail -n +$((FROM_I + 1)) | head -n ${NUM_FILES}))
+
+# Check if there are any matching files
+if [ ${#root_files[@]} -eq 0 ]; then
+    echo "No root files found matching the pattern."
+    exit 1
+fi
+
+
+# Copy each file using the Python script
+for file in "${root_files[@]}"; do
+    echo "Copying file ${file} to ${destination_dir}..."
+    python3 /afs/cern.ch/work/f/fccsw/public/FCCutils/eoscopy.py "${file}" "${destination_dir}/$(basename ${file})"
+done
+
+# merge 1000 root files to one input file
+hadd merged_input.root ins.root
+python create_jet_based_tree.py merged_input.root out.root
+
+# make directory if it does not exist:
+if [ ! -d ${OUTPUT_DIR} ]; then
+  mkdir -p ${OUTPUT_DIR}
+fi
+
+# copy file to output dir
+python /afs/cern.ch/work/f/fccsw/public/FCCutils/eoscopy.py out.root ${OUTPUTFILE}
+echo "copying output ... "
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 echo "LOCAL_DIR dir: ${LOCAL_DIR}"
 
@@ -43,8 +88,8 @@ which fccanalysis
 echo ${SCRIPT}
 fccanalysis run ${SCRIPT} --output out.root --files-list ${PWD}/in.root --nevents ${NEVENTS}
 #fccanalysis run script.py --output out.root --files-list in.root --nevents ${NEVENTS}
+python create_fullsim_tagger_data.py ${INPUT_FILE_PATTERN} ${OUTPUT_DIR} ${OUTPUT_FILE} ${START_IND} ${END_IND}
 echo "job done ... "
 python /afs/cern.ch/work/f/fccsw/public/FCCutils/eoscopy.py out.root ${OUTPUTFILE}
 echo "copying output ... "
-                                                                                                                                            45,1          Bot
 
